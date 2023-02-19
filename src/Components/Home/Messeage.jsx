87d5cc69f7477a00d311f70b59947d7c0b15fdd1
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-02-19 17:56:23 +0330

    form

--- src/Components/Home/Messeage.jsx
+++ src/Components/Home/Messeage.jsx
@@ -6,12 +6,31 @@ export default function Messeage() {
     <div id="messeage">
       <h2 style={{ color: "black" }}>SEND ME A MESSAGE</h2>
 
-      <form className="messeage__form">
-        <input placeholder="Your name" type="text" className="my-input" />
-        <input placeholder="Your Email" type="text" className="my-input" />
-        <input placeholder="Subject" type="text" className="my-input" />
+      <form
+        action="https://formspree.io/f/xeqwgaak"
+        method="POST"
+        className="messeage__form"
+      >
+        <input
+          name="name"
+          placeholder="Your name"
+          type="text"
+          className="my-input"
+        />
+        <input
+          name="email"
+          placeholder="Your Email"
+          type="text"
+          className="my-input"
+        />
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          className="my-input"
+        />
         <textarea
-          name=""
+          name="messeage"
           placeholder="Messeage"
           className="my-input"
           id=""
